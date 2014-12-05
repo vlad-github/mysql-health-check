ba28b5acd7caec182c10bfec100317c3c2bdda72
astellar1 <vlad@astellar.com>
2014-12-05 15:30:05 +0300

    Report title fixed

--- check_run.sh
+++ check_run.sh
@@ -40,14 +40,14 @@ if [ "$1" == '--email' ] ; then
 	if [ ! -z "$2" ] ; then
 		### send output to EMAIL provided
 		EMAIL=$2
-		./mysql_health_check.sh $HOSTNAME $MYSQL_HOST $MYSQL_USER "$MYSQL_PASS" | mail -s "Health check for $HOSTNAME" $EMAIL
+		./mysql_health_check.sh $HOSTNAME $MYSQL_HOST $MYSQL_USER "$MYSQL_PASS" | mail -s "MySQL health check summary report for $HOSTNAME" $EMAIL
 	else
 		usage
 		exit 1
 	fi 
 else
 
-echo "Health check for $HOSTNAME"
+echo "MySQL health check summary report for $HOSTNAME"
 ./mysql_health_check.sh $HOSTNAME $MYSQL_HOST $MYSQL_USER "$MYSQL_PASS"
 
 fi
