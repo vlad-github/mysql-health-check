ba4ea41c4c152b0dacd1a39ad6d4bf2bc6d022b7
astellar1 <vlad@astellar.com>
2014-12-09 13:46:05 +0300

    --first-look data is now stored in ./review dir

--- first_look.sh
+++ first_look.sh
@@ -37,56 +37,56 @@ PT_BIN_PATH="./bin"
 ### Preparing for start
 CMDL_PASS="--password=$MPASS --port=$MPORT"
 
-mkdir -p $MNAME
-if [ ! -d $MNAME ]; then
-    echo "Can't create output directory: $DIGEST_DIR"
+REVIEW_DIR=review
+
+mkdir -p $REVIEW_DIR
+if [ ! -d $REVIEW_DIR ]; then
+    echo "Can't create output directory: $REVIEW_DIR"
     exit 1
 fi
-
-### FIXME add error handling for directory creation
 
 if [ "$MHOST" == 'localhost' ] || [ "$MHOST" == '127.0.0.1' ] || [ "$MHOST" == '0' ] ; then
     echo "Collecting local data (system summary) for host=$MHOST system name=$MNAME:"
 
     echo -n "Gathering system summary..."
-    $PT_BIN_PATH/pt-summary > $MNAME/sys-pt-summary.log
+    $PT_BIN_PATH/pt-summary > $REVIEW_DIR/sys-pt-summary.log
 
     echo -n "iostat..."
-    iostat -dx 10 3 > $MNAME/sys-iostat.log
+    iostat -dx 10 3 > $REVIEW_DIR/sys-iostat.log
 
     echo -n "vmstat..."
-    vmstat 10 3 > $MNAME/sys-vmstat.log
+    vmstat 10 3 > $REVIEW_DIR/sys-vmstat.log
     echo "Done."
 fi
 
 echo "Collecting MySQL server data for host=$MHOST:"
 echo -n "Mysql summary..."
-$PT_BIN_PATH/pt-mysql-summary -- -h $MHOST -u $MUSER $CMDL_PASS > $MNAME/db-mysql-summary.log
+$PT_BIN_PATH/pt-mysql-summary -- -h $MHOST -u $MUSER $CMDL_PASS > $REVIEW_DIR/db-mysql-summary.log
 
 echo -n "Live counters (20 seconds)..."
-mysqladmin -h $MHOST -u $MUSER $CMDL_PASS -r -c 3 -i 10 extended-status > $MNAME/db-stats.log
+mysqladmin -h $MHOST -u $MUSER $CMDL_PASS -r -c 3 -i 10 extended-status > $REVIEW_DIR/db-stats.log
 echo "Done";
 
 echo "Fetching Tables and Egines statistics:"
 
 echo "1. Getting per-engine distribution..."
-mysql -t -h $MHOST -u $MUSER $CMDL_PASS -e "SELECT engine, count(*) TABLES,  concat(round(sum(table_rows)/1000000,2),'M') rows, concat(round(sum(data_length)/(1024*1024*1024),2),'G') DATA, concat(round(sum(index_length)/(1024*1024*1024),2),'G') idx, concat(round(sum(data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(sum(index_length)/sum(data_length),2) idxfrac FROM information_schema.TABLES WHERE table_schema NOT IN ('mysql', 'information_schema', 'performance_schema') GROUP BY engine ORDER BY sum(data_length+index_length) DESC LIMIT 10" > $MNAME/db-engines.log
+mysql -t -h $MHOST -u $MUSER $CMDL_PASS -e "SELECT engine, count(*) TABLES,  concat(round(sum(table_rows)/1000000,2),'M') rows, concat(round(sum(data_length)/(1024*1024*1024),2),'G') DATA, concat(round(sum(index_length)/(1024*1024*1024),2),'G') idx, concat(round(sum(data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(sum(index_length)/sum(data_length),2) idxfrac FROM information_schema.TABLES WHERE table_schema NOT IN ('mysql', 'information_schema', 'performance_schema') GROUP BY engine ORDER BY sum(data_length+index_length) DESC LIMIT 10" > $REVIEW_DIR/db-engines.log
 
 echo "2. Getting TOP 10 largest tables by size..."
-mysql -t -h $MHOST -u $MUSER $CMDL_PASS -e "SELECT concat(table_schema,'.',table_name), engine,  concat(round(table_rows/1000000,2),'M') rows, concat(round(data_length/(1024*1024*1024),2),'G') DATA, concat(round(index_length/(1024*1024*1024),2),'G') idx, concat(round((data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(index_length/data_length,2) idxfrac FROM information_schema.TABLES ORDER BY data_length+index_length DESC LIMIT 10" > $MNAME/db-top-tables.log
+mysql -t -h $MHOST -u $MUSER $CMDL_PASS -e "SELECT concat(table_schema,'.',table_name), engine,  concat(round(table_rows/1000000,2),'M') rows, concat(round(data_length/(1024*1024*1024),2),'G') DATA, concat(round(index_length/(1024*1024*1024),2),'G') idx, concat(round((data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(index_length/data_length,2) idxfrac FROM information_schema.TABLES ORDER BY data_length+index_length DESC LIMIT 10" > $REVIEW_DIR/db-top-tables.log
 
 echo "3. Getting tables size"
-mysql -t -h $MHOST -u $MUSER $CMDL_PASS -e "SELECT concat(table_schema,'.',table_name), engine,  concat(round(table_rows/1000000,2),'M') rows, concat(round(data_length/(1024*1024*1024),2),'G') DATA, concat(round(index_length/(1024*1024*1024),2),'G') idx, concat(round((data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(index_length/data_length,2) idxfrac FROM information_schema.TABLES WHERE table_schema NOT IN ('mysql', 'information_schema', 'performance_schema') ORDER BY table_schema ASC, data_length+index_length DESC" > $MNAME/db-all-tables.log
+mysql -t -h $MHOST -u $MUSER $CMDL_PASS -e "SELECT concat(table_schema,'.',table_name), engine,  concat(round(table_rows/1000000,2),'M') rows, concat(round(data_length/(1024*1024*1024),2),'G') DATA, concat(round(index_length/(1024*1024*1024),2),'G') idx, concat(round((data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(index_length/data_length,2) idxfrac FROM information_schema.TABLES WHERE table_schema NOT IN ('mysql', 'information_schema', 'performance_schema') ORDER BY table_schema ASC, data_length+index_length DESC" > $REVIEW_DIR/db-all-tables.log
 
 echo "4. Getting current InnoDB engine status..."
-mysql -h $MHOST -u $MUSER $CMDL_PASS -e "SHOW ENGINE INNODB STATUS\G" > $MNAME/db-innodb.log
+mysql -h $MHOST -u $MUSER $CMDL_PASS -e "SHOW ENGINE INNODB STATUS\G" > $REVIEW_DIR/db-innodb.log
 
 echo "5. Getting current process list..."
-mysql -h $MHOST -u $MUSER $CMDL_PASS -e "SHOW PROCESSLIST\G" > $MNAME/db-processlist.log
+mysql -h $MHOST -u $MUSER $CMDL_PASS -e "SHOW PROCESSLIST\G" > $REVIEW_DIR/db-processlist.log
 echo "Done. Packing data.";
 
 ### Archive collected data
-tar -zcvf $MNAME.tar.gz $MNAME/*
+tar -zcvf $REVIEW_DIR.tar.gz $REVIEW_DIR/*
 
 ### Uncomment here to send archive to <email@address.com>:
-#base64 $MNAME.tar.gz | mail -s "review $MNAME" <email@address.com>
+#base64 $REVIEW_DIR.tar.gz | mail -s "review $MNAME" <email@address.com>
