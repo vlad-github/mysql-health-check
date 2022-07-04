3973799ea32dfde15d1b2efc25c21632de4a330b
Vladimir Fedorkov <v.fedorkov@it.ecommpay.com>
2022-07-04 10:16:22 +0000

    Added query statistics gather from the box

--- first_look.sh
+++ first_look.sh
@@ -65,41 +65,47 @@ fi
 if [ "$MHOST" == 'localhost' ] || [ "$MHOST" == '127.0.0.1' ] || [ "$MHOST" == '0' ] ; then
     echo "Collecting local data (system summary) for host=$MHOST system name=$MNAME:"
 
-    echo -n "Gathering system summary..."
+    echo -n " 1. Gathering system summary..."
     $PT_BIN_PATH/pt-summary > $REVIEW_DIR/sys-pt-summary.log
 
-    echo -n "iostat..."
+    echo -n " 2. iostat..."
     iostat -dx 10 3 > $REVIEW_DIR/sys-iostat.log
 
-    echo -n "vmstat..."
+    echo -n " 3. vmstat..."
     vmstat 10 3 > $REVIEW_DIR/sys-vmstat.log
     echo "Done."
 fi
 
 echo "Collecting MySQL server data for host=$MHOST:"
-echo -n "Mysql summary..."
+echo -n " 4. Mysql summary..."
 $PT_BIN_PATH/pt-mysql-summary -- $CMDL_DSN > $REVIEW_DIR/db-mysql-summary.log
 
-echo -n "Live counters (20 seconds)..."
+echo -n " 5. Live counters (20 seconds)..."
 mysqladmin $CMDL_DSN -r -c 3 -i 10 extended-status > $REVIEW_DIR/db-stats.log
 echo "Done";
 
 echo "Fetching MySQL tables and egines statistics:"
 
-echo "1. Getting per-engine distribution..."
+echo "6. Getting per-engine distribution..."
 mysql -t $CMDL_DSN -e "SELECT engine, count(*) TABLES,  concat(round(sum(table_rows)/1000000,2),'M') rows, concat(round(sum(data_length)/(1024*1024*1024),2),'G') DATA, concat(round(sum(index_length)/(1024*1024*1024),2),'G') idx, concat(round(sum(data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(sum(index_length)/sum(data_length),2) idxfrac FROM information_schema.TABLES WHERE table_schema NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys') GROUP BY engine ORDER BY sum(data_length+index_length) DESC LIMIT 10" > $REVIEW_DIR/db-engines.log
 
-echo "2. Getting TOP 10 largest tables by size..."
+echo "7. Getting TOP 10 largest tables by size..."
 mysql -t $CMDL_DSN -e "SELECT concat(table_schema,'.',table_name), engine,  concat(round(table_rows/1000000,2),'M') rows, concat(round(data_length/(1024*1024*1024),2),'G') DATA, concat(round(index_length/(1024*1024*1024),2),'G') idx, concat(round((data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(index_length/data_length,2) idxfrac FROM information_schema.TABLES ORDER BY data_length+index_length DESC LIMIT 10" > $REVIEW_DIR/db-top-tables.log
 
-echo "3. Getting tables size"
+echo "8. Getting tables size"
 mysql -t $CMDL_DSN -e "SELECT concat(table_schema,'.',table_name), engine,  concat(round(table_rows/1000000,2),'M') rows, concat(round(data_length/(1024*1024*1024),2),'G') DATA, concat(round(index_length/(1024*1024*1024),2),'G') idx, concat(round((data_length+index_length)/(1024*1024*1024),2),'G') total_size, round(index_length/data_length,2) idxfrac FROM information_schema.TABLES WHERE table_schema NOT IN ('mysql', 'information_schema', 'performance_schema', 'sys') ORDER BY table_schema ASC, data_length+index_length DESC" > $REVIEW_DIR/db-all-tables.log
 
-echo "4. Getting current InnoDB engine status..."
+echo "9. Getting current InnoDB engine status..."
 mysql $CMDL_DSN -e "SHOW ENGINE INNODB STATUS\G" > $REVIEW_DIR/db-innodb.log
 
-echo "5. Getting current process list..."
+echo "10. Getting current process list..."
 mysql $CMDL_DSN -e "SHOW PROCESSLIST\G" > $REVIEW_DIR/db-processlist.log
+
+echo "11. Collecting top queries by time utilization..."
+mysql $CMDL_DSN -e "SELECT SCHEMA_NAME, SUBSTR(DIGEST_TEXT, 1, 512) as query, COUNT_STAR, CONCAT('Rows scanned: ', SUM_ROWS_EXAMINED, ', Sent: ', SUM_ROWS_SENT) as rows_stats, ROUND(SUM_ROWS_SENT / COUNT_STAR, 6) as rows_per_query, FLOOR(SUM_ROWS_EXAMINED / SUM_ROWS_SENT) as efficiency_rate, CONCAT('Full scans: ', SUM_SELECT_SCAN, ', Range scans: ', SUM_SELECT_RANGE, ', Index scans: ', SUM_NO_INDEX_USED) as select_stats, CONCAT('Tmp tables: ', SUM_CREATED_TMP_TABLES, ', On disk tmp tables: ', SUM_CREATED_TMP_DISK_TABLES) as tmp_tables_stat, FIRST_SEEN, LAST_SEEN, ROUND(MAX_TIMER_WAIT / 1000000000000 , 6) as max_time, ROUND(SUM_TIMER_WAIT / COUNT_STAR / 1000000000000 , 6) as avg_time, ROUND(SUM_LOCK_TIME / COUNT_STAR / 1000000000000 , 6) as avg_lock_time, ROUND(COUNT_STAR / (UNIX_TIMESTAMP(LAST_SEEN) - UNIX_TIMESTAMP(FIRST_SEEN)), 0) as avg_qps FROM performance_schema.events_statements_summary_by_digest ORDER BY SUM_TIMER_WAIT desc limit 100\G" > $REVIEW_DIR/db-top-quries-by-time.log
+
+echo "12. Collecting top queries by frequency..."
+mysql $CMDL_DSN -e "SELECT SCHEMA_NAME, SUBSTR(DIGEST_TEXT, 1, 512) as query, COUNT_STAR, CONCAT('Rows scanned: ', SUM_ROWS_EXAMINED, ', Sent: ', SUM_ROWS_SENT) as rows_stats, ROUND(SUM_ROWS_SENT / COUNT_STAR, 6) as rows_per_query, FLOOR(SUM_ROWS_EXAMINED / SUM_ROWS_SENT) as efficiency_rate, CONCAT('Full scans: ', SUM_SELECT_SCAN, ', Range scans: ', SUM_SELECT_RANGE, ', Index scans: ', SUM_NO_INDEX_USED) as select_stats, CONCAT('Tmp tables: ', SUM_CREATED_TMP_TABLES, ', On disk tmp tables: ', SUM_CREATED_TMP_DISK_TABLES) as tmp_tables_stat, FIRST_SEEN, LAST_SEEN, ROUND(MAX_TIMER_WAIT / 1000000000000 , 6) as max_time, ROUND(SUM_TIMER_WAIT / COUNT_STAR / 1000000000000 , 6) as avg_time, ROUND(SUM_LOCK_TIME / COUNT_STAR / 1000000000000 , 6) as avg_lock_time, ROUND(COUNT_STAR / (UNIX_TIMESTAMP(LAST_SEEN) - UNIX_TIMESTAMP(FIRST_SEEN)), 0) as avg_qps FROM performance_schema.events_statements_summary_by_digest ORDER BY COUNT_STAR desc limit 100\G" > $REVIEW_DIR/db-top-quries-by-count.log
 
 echo "Done, paking data";
 tar -zcvf $REVIEW_DIR.tar.gz $REVIEW_DIR/*
